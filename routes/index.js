71444438e58fa96ce1a716bcf62fb3cd28a242bc
PORJOLOVSKY MARTÍN LEONARDO <44261597@ort>
2018-10-12 11:20:29 -0300

    prueba con axel

--- routes/index.js
+++ routes/index.js
@@ -41,6 +41,54 @@ router.get('/solicitudes', function(req, res){
 
 router.get('/agregarUsuarios', function (req, res){
     res.sendFile(path.join(__dirname, '../views/agregarUsuarios.html'));
+
+    console.log("asd");
+    console.log("info recibida del posteo");
+    var id = req.body.DniUsuario; 
+    var Nombre = req.body.NombreUsuario;
+    var apellido = req.body.ApellidoUsuario;
+    var contraseña = req.body.ContraseñaUsuario;
+    if(Nombre === "undefined" || apellido || "undefined" | id === "undefined" || id === "" || contraseña === "undefined"){
+    Console.log("Datos no especificados");
+    }else{
+    console.log("Datos guardados y especificados");
+    }
+    
+    if (Nombre != "" && apellido != "" && id !="" && contraseña != "") { 
+        console.log("info recibida del posteo y se agrego el usuario")
+    var query = connection.query(" SELECT * FROM `users` WHERE DNI ='" + id + "'" ,function(error,result){
+        if(error){
+        throw error;
+    }else{
+        cantidad = result;
+        if(cantidad >= 1){
+            console.log("Ya exsiste el usuario");
+        } else {
+            if(id === ""){
+            alert("completar campo faltante");
+            console.log("No se agrego el usuario");
+            }else if (Nombre === "" ){
+            alert("completar campo faltante");
+            console.log("No se agrego el usuario");
+            }else if (contraseña === ""){
+            alert("completar campo faltante");
+            console.log("No se agrego el usuario");
+            }else{
+            {
+                // agregar apellido 
+                console.log();
+                connection.query(`INSERT INTO \``+ "users" +`\` (\``+ "DNI" +`\`, \``+ "username" +`\`, \``+"password"+`\`, \``+"role"+`\` ) VALUES ('`+ "  "+`', '`+id+`','`+contraseña+`','Teacher')`, function (error, results, fields) {
+                console.log("se a guardado el usuario");
+            })     
+            }
+            }
+        }
+        }
+        }
+    )
+    } else {
+    res.status(403).send({error: 'Completar campos faltantes.'})
+    }
 }
 
 );
@@ -48,14 +96,6 @@ router.get('/agregarUsuarios', function (req, res){
 
 
 router.post('/agregarUsuarios', function (req, res) {
-    console.log("asd");
-    console.log("info recibida del posteo");
-    var id = req.body.DniUsuario || "" ; 
-    var Nombre = req.body.NombreUsuario || "" ;
-    var apellido = req.body.ApellidoUsuario || "" ;
-    var contraseña = req.body.ContraseñaUsuario || "" ;
-    console.log(id,Nombre,apellido,contraseña,"juampi es un capo")
-    agregarUsuario(id,Nombre,apellido,contraseña,res);
 }); 
 
 
@@ -65,70 +105,30 @@ router.get('/VentanaAdmin', function (req, res){
 
 
 
+
+
 module.exports = router;
- 
-function agregarUsuario(id, Nombre, apellido, contraseña, response){
-    
-        console.log("Datos guardados y especificados");
-        if (Nombre != "" && apellido != "" && id !="" && contraseña != "") { 
-            console.log("info recibida del posteo y se agrego el usuario")
-        var query = connection.query(" SELECT * FROM `users` WHERE DNI ='" + id + "'" ,function(error,result){
-            if(error){
-            throw error;
-        }else{
-            cantidad = result;
-            if(cantidad >= 1){
-                console.log("Ya exsiste el usuario");
-            } else {
-                if(id === ""){
-                alert("completar campo faltante");
-                console.log("No se agrego el usuario");
-                }else if (Nombre === "" ){
-                alert("completar campo faltante");
-                console.log("No se agrego el usuario");
-                }else if (contraseña === ""){
-                alert("completar campo faltante");
-                console.log("No se agrego el usuario");
-                }else{ 
-                    connection.query(`INSERT INTO \``+ "users" +`\` (\``+ "DNI" +`\`, \``+ "username" +`\`, \``+"password"+`\`, \``+"role"+`\` ) VALUES ('`+id+`', '`+Nombre+`','`+contraseña+`','Teacher')`, function (error, results, fields) {
-                     console.log("se a guardado el usuario");
-                }); 
-                    connection.query(`INSERT INTO \``+ "teachers" +`\` (\``+ "id" +`\`, \``+ "firstname" +`\`, \``+"lastname"+`\`, \``+"DNIusers"+`\` ) VALUES ('`+ " " +`', '`+Nombre+`','`+"Udentifed:)"+`','`+id+`'`, function (error, results, fields) {
-                    console.log("se a guardado el maestro");
-                });   
-                response.redirect('/VentanaAdmin');
-            }
-            
-            
-            }
-            }
-            }
-        
-        )
-        } else {
-        response.status(403).send({error: 'Completar campos faltantes.'})
-        }
-    }
 
 
 
 
 
-
-/*
 
 function ValidarUsuario(dni, password, response){
-    var sql = "SELECT name, lastName FROM users WHERE dni = "+dni+" AND password = '"+password+"'";
+    var sql = "SELECT name, lastName, role FROM users WHERE dni = "+dni+" AND password = '"+password+"'";
     connection.query(sql, function(error, result){
+        var role = result[0].role;
         if (error) throw error;
         if (result.length === 0){
             console.log('No existe el usuario');
             return false;
         }else{
-            console.log(result);
-            response.redirect('/seleccion-dias');
-            return true;
+            if(role === "teacher"){
+                response.redirect('/seleccion-dias');
+                return true;
+            }else{
+                response.redirect('/VentanaAdmin');
+            }
         }
     });
 } 
-*/
