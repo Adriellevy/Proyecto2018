975867570be7e413e1e4b52b7138c08fae0cc0bc
Adriellevy <adriellevy2014@gmail.com>
2018-10-17 17:07:01 -0300

    adri

--- routes/index.js
+++ routes/index.js
@@ -14,19 +14,28 @@ router.get('/login', function (req, res) {
     res.sendFile(path.join(__dirname, '../views/inicioSesion.html'));
 });
 
-router.post('/login', function (req, res){
-  console.log('post');
-  var dni = req.body.dni;
-  var password = req.body.password;
-  console.log(dni);
-  console.log(password);
-  //res.redirect('/seleccion-dias');
-  ValidarUsuario(dni, password, res);
-});
-
 router.get('/solicitudesProf', function(req, res){
     res.sendFile(path.join(__dirname, '../views/seleccionDias.html'));
 });
+
+router.get('/solisitudesAdm',function(req,res){
+    res.sendFile(path.join(__dirname, '../views/solicitudes.html'));
+})
+
+router.get('/agregarUsuarios', function (req, res){
+    res.sendFile(path.join(__dirname, '../views/agregarUsuarios.html'));
+});
+
+
+router.post('/login', function (req, res){
+    console.log('post');
+    var dni = req.body.dni;
+    var password = req.body.password;
+    console.log(dni);
+    console.log(password);
+    //res.redirect('/seleccion-dias');
+    ValidarUsuario(dni, password, res);
+  });
 
 router.post('/solicitudesProf',function(req,res){
     var Día = req.body.Listadias || "null" ; 
@@ -36,9 +45,10 @@ router.post('/solicitudesProf',function(req,res){
     BuscarAula(Día,Bloque,res);
 });
 
-router.get('/agregarUsuarios', function (req, res){
-    res.sendFile(path.join(__dirname, '../views/agregarUsuarios.html'));
+router.post('/SolisitudesAdm',function(req,res){
+    SolicitudesAdm();
 });
+
 router.post('/agregarUsuarios', function (req, res) {
     console.log("info recibida del posteo");
     var id = req.body.DniUsuario || "" ; 
@@ -49,64 +59,56 @@ router.post('/agregarUsuarios', function (req, res) {
     agregarUsuario(id,Nombre,apellido,contraseña,res);
 }); 
 
-
 router.get('/VentanaAdmin', function (req, res){
     res.sendFile(path.join(__dirname, '../views/ventanaAdmin.html'));
     console.log("una computadora se ha conectado a la pagina /ventanaAdmin ")
 });
 
-
-
 module.exports = router;
  
+
 function agregarUsuario(id, Nombre, apellido, contraseña, response){
-        console.log("Datos guardados y especificados");
-        if (Nombre != "" && apellido != "" && id !="" && contraseña != "") { 
-            console.log("info recibida del posteo y se agrego el usuario")
-        var query = connection.query(" SELECT * FROM `users` WHERE DNI ='" + id + "'" ,function(error,result){
-            if(error){
-            throw error;
-        }else{
-            cantidad = result;
-            if(cantidad >= 1){
-                console.log("Ya exsiste el usuario");
-            } else {
-                if(id === ""){
-                alert("completar campo faltante");
-                console.log("No se agrego el usuario");
-                }else if (Nombre === "" ){
-                alert("completar campo faltante");
-                console.log("No se agrego el usuario");
-                }else if (contraseña === ""){
-                alert("completar campo faltante");
-                console.log("No se agrego el usuario");
-                }else{ 
-                    connection.query(`INSERT INTO \``+ "users" +`\` (\``+ "DNI" +`\`, \``+ "username" +`\`, \``+"password"+`\`, \``+"role"+`\` ) VALUES ('`+id+`', '`+Nombre+`','`+contraseña+`','Teacher')`, function (error, results, fields) {
-                     console.log("se a guardado el usuario");
-                }); 
-                    connection.query(`INSERT INTO \``+ "teachers" +`\` (\``+ "id" +`\`, \``+ "firstname" +`\`, \``+"lastname"+`\`, \``+"DNIusers"+`\` ) VALUES ('`+ " " +`', '`+Nombre+`','`+"Udentifed:)"+`','`+id+`'`, function (error, results, fields) {
-                    console.log("se a guardado el maestro");
-                });   
-                response.redirect('/VentanaAdmin');
-                }          
-              }
-            }
-          })
+    console.log("Datos guardados y especificados");
+    if (Nombre != "" && apellido != "" && id !="" && contraseña != "") { 
+        console.log("info recibida del posteo y se agrego el usuario")
+    var query = connection.query(" SELECT * FROM `users` WHERE DNI ='" + id + "'" ,function(error,result){
+        if(error){
+        throw error;
+    }else{
+        cantidad = result;
+        if(cantidad >= 1){
+            console.log("Ya exsiste el usuario");
         } else {
-        response.status(403).send({error: 'Completar campos faltantes.'})
+            if(id === ""){
+            alert("completar campo faltante");
+            console.log("No se agrego el usuario");
+            }else if (Nombre === "" ){
+            alert("completar campo faltante");
+            console.log("No se agrego el usuario");
+            }else if (contraseña === ""){
+            alert("completar campo faltante");
+            console.log("No se agrego el usuario");
+            }else{ 
+                connection.query(`INSERT INTO \``+ "users" +`\` (\``+ "DNI" +`\`, \``+ "username" +`\`, \``+"password"+`\`, \``+"role"+`\` ) VALUES ('`+id+`', '`+Nombre+`','`+contraseña+`','Teacher')`, function (error, results, fields) {
+                 console.log("se a guardado el usuario");
+            }); 
+                connection.query(`INSERT INTO \``+ "teachers" +`\` (\``+ "id" +`\`, \``+ "firstname" +`\`, \``+"lastname"+`\`, \``+"DNIusers"+`\` ) VALUES ('`+ " " +`', '`+Nombre+`','`+"Udentifed:)"+`','`+id+`'`, function (error, results, fields) {
+                console.log("se a guardado el maestro");
+            });   
+            response.redirect('/VentanaAdmin');
+            }          
+          }
         }
+      })
+    } else {
+    response.status(403).send({error: 'Completar campos faltantes.'})
     }
+}
 
 
 router.get('/VentanaAdmin', function (req, res){
-    res.sendFile(path.join(__dirname, '../views/ventanaAdmin.html'));
+res.sendFile(path.join(__dirname, '../views/ventanaAdmin.html'));
 });
-
-
-
-
-
-module.exports = router;
 
 
 
@@ -133,11 +135,19 @@ function ValidarUsuario(dni, password, response){
 } 
 
 
-    function SolicitudesAula(){
-    
-    }
 
-    function BuscarAula(Día,Bloque,response){
+
+function SolicitudesAdm(){
+
+    
+}
+
+
+
+
+
+
+function BuscarAula(Día,Bloque,response){
         var bloquefinal; 
         if (Bloque === "1 Bloque" && Día === "Lunes" ){
             bloquefinal = 1;
@@ -186,7 +196,7 @@ function ValidarUsuario(dni, password, response){
         }
         console.log("El bloque final queda asi: " + bloquefinal);
     try{
-        connection.query(`SELECT * FROM ` + "schedule" + ``+` WHERE `+"block"+` = `+ bloquefinal +` && `+"status" +` = 1`, function (error, results, fields) {
+        connection.query(`SELECT * FROM ` + "schedule" + ``+` WHERE `+"block"+` = `+ bloquefinal +` && `+"status" +` = 0`, function (error, results, fields) {
                 if(error || results === 0 || results === null || results === []){
                    console.log("no hay horario disponible");
                    throw error;  
@@ -210,7 +220,18 @@ function ValidarUsuario(dni, password, response){
                                                 nombreaula = json1[0].name; 
                                                 console.log('>> room.name: ' + nombreaula);
                                                 //export(nombreaula);
-                                                
+                                                exports.nombre = nombreaula;
+                                                var botonOn = require('./module');
+                                                if (botonOn === "ON"){
+                                                    connection.query(`UPDATE ` + "schedule" +` SET ` + "status" + `= 2 `+" && block ="+bloquefinal+` WHERE `+ "status" +`= 0 && `+"idroom"+`=`+ IDroom +``, function (error, result, fields){
+                                                        if(error){
+                                                            throw(err);
+                                                        }
+                                                        else{ 
+                                                            console.log("se a actualizado el estado del aula");
+                                                        }
+                                                    });
+                                                }
                                             }
                                         });
                             }                        
@@ -224,3 +245,4 @@ function ValidarUsuario(dni, password, response){
                 console.log(error)
         }
         } 
+
